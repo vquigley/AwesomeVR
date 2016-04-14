f520ef07f4dd0c4fe0c4be774f7bcfe4ef737ea0
vquigley <vinny_182@hotmail.com>
2016-04-14 12:18:32 +0100

    bug in index.js

--- controllers/index.js
+++ controllers/index.js
@@ -20,7 +20,7 @@ module.exports = function (router) {
         res.send('Error, wrong validation token');
     });
     
-    app.post('/api/messenger', function (req, res) {
+    router.post('/api/messenger', function (req, res) {
         messaging_events = req.body.entry[0].messaging;
         for (i = 0; i < messaging_events.length; i++) {
             event = req.body.entry[0].messaging[i];
