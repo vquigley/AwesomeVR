5eda0ff4683c345e7681bff6b735715d49c63f59
vquigley <vinny_182@hotmail.com>
2016-04-14 12:13:09 +0100

    bot response

--- controllers/index.js
+++ controllers/index.js
@@ -6,16 +6,54 @@ var IndexModel = require('../models/index');
 module.exports = function (router) {
 
     var model = new IndexModel();
-
+    
     router.get('/', function (req, res) {
         res.render('index', model);
     });
     
+    var token = "I_R_BABOON";
+    
     router.get('/api/messenger', function (req, res) {
-        if (req.query['hub.verify_token'] === 'I_R_BABOON') {
+        if (req.query['hub.verify_token'] === token) {
             res.send(req.query['hub.challenge']);
         }
         res.send('Error, wrong validation token');
     });
     
+    app.post('/api/messenger', function (req, res) {
+        messaging_events = req.body.entry[0].messaging;
+        for (i = 0; i < messaging_events.length; i++) {
+            event = req.body.entry[0].messaging[i];
+            sender = event.sender.id;
+            if (event.message && event.message.text) {
+            text = event.message.text;
+             sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+            }
+        }
+        res.sendStatus(200);
+    });  
+
+    function sendTextMessage(sender, text) {
+        messageData = {
+            text:text
+        }
+        request(
+            {
+                url: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: {access_token:token},
+                method: 'POST',
+                json: {
+                    recipient: {id:sender},
+                    message: messageData,
+                }
+            }, 
+            function(error, response, body) {
+                if (error) {
+                    console.log('Error sending message: ', error);
+                } else if (response.body.error) {
+                    console.log('Error: ', response.body.error);
+                }
+            }
+        );
+    }
 };
